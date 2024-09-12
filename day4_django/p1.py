b7cecb5e09c79f6ad27fe54c5662d28b8e6ab0f3
Nithin NellakantaRao <mtd.nithin@gmail.com>
2024-09-12 10:25:17 +0530

    p4 and p5 programs added

--- day4_django/p1.py
+++ day4_django/p1.py
@@ -3,7 +3,7 @@ import pymysql
 def connectDb():
     connectionObj = pymysql.Connect(
             host='localhost', port=3306,
-            user='root', password='Root123456',
+            user='root', password='Root123',
             db='nithin_db', charset='utf8')
     print('Database connected successfully')
     return connectionObj
